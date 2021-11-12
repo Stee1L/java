41607eb90d72f7a0a916a59c968047d2e875fbbe
Stee1L <79190996+Stee1L@users.noreply.github.com>
2021-11-13 04:44:26 +0700

    Update DesktopComputer.java

--- lab1/DesktopComputer.java
+++ lab1/DesktopComputer.java
@@ -2,13 +2,12 @@ public class DesktopComputer extends Computer{
     /** attributes */
     private int ScreenWidth, ScreenHeight;
     /** constructors */
-    //public DesktopComputer(){}
     public DesktopComputer(int size, String model, double price, int screenWidth, int screenHeight)
     {
         super(size, model, price);
-        if (screenWidth <= 0) throw new IllegalAgumentException("Size must be greater then zero");
+        if (screenWidth <= 0) throw new IllegalArgumentException("Size must be greater then zero");
         else ScreenHeight = screenHeight;
-        if (screenHeight <= 0) throw new IllegalAgumentException("Size must be greater then zero");
+        if (screenHeight <= 0) throw new IllegalArgumentException("Size must be greater then zero");
         else ScreenWidth = screenWidth;
     }
     /** setters & getters */
