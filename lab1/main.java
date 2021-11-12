9c7e032a2ec7ea207c2721fcbe952f06991d53eb
Stee1L <79190996+Stee1L@users.noreply.github.com>
2021-11-13 04:44:53 +0700

    Update main.java

--- lab1/main.java
+++ lab1/main.java
@@ -5,8 +5,13 @@ public class main {
         HardDrive AData = new HardDrive(512000);
         Computer PC1 = new Computer(size, "Lenovo", price);
         DesktopComputer PC2 = new DesktopComputer(2000000, "HP", 100000, 1920, 1080);
+        AData.setSize(256000);
         System.out.println("AData size: " + AData.getSize());
+        PC1.setModel("MSI");
         System.out.println("PC1 model: " + PC1.getModel());
         PC2.getInfo();
+        PC2.setScreenHeight(2160);
+        PC2.setScreenWidth(3840);
+        System.out.print("\nPC2 resolution is " + PC2.getScreenWidth() + " x " + PC2.getScreenHeight());
     }
 }
